6b8fc59da7a2a8fe3caa3cabb28bd297d33e3a7f
Anna <annadeng2020@u.northwestern.edu>
2018-02-25 17:16:36 -0600

    updates from anna filter rip

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -3,6 +3,19 @@ var skillsDropdown = document.getElementById('skills-dropdown');
 var causesBox = document.getElementById('causes');
 var causesDropdown = document.getElementById('causes-dropdown');
 
+var nameplace = document.getElementById('devname');
+var name = document.getElementById('defaultForm-name');
+var emailplace = document.getElementById('devemail');
+var email = document.getElementById('defaultForm-email');
+
+function setName(){
+  nameplace.value = name.value;
+}
+
+function setEmail(){
+emailplace.value = email.value;
+}
+
 skillsDropdown.onchange = function(){
      skillsBox.value = skillsBox.value  + this.value + ", ";
 }
