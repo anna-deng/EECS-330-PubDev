79571ea65087eb354516152477669f49dd50600a
rohitrastogi <rohitrastogi2019@u.northwestern.edu>
2018-02-25 21:04:45 -0600

    fixing bad merge

--- scripts/direct.js
+++ scripts/direct.js
@@ -1,5 +1,4 @@
 function filtercards(){
-<<<<<<< HEAD
     var cards = Array.from(document.getElementsByClassName('card'));
 
     //setting display none before filtering
@@ -77,40 +76,6 @@ function filtercards(){
     //set display none for remaining items
     for(var k= 0; k<removedCards.length; k++){
         removedCards[k].style.display = "none";
-=======
-  var cards = Array.from(document.getElementsByClassName('card'));
-  var causechecks = Array.from(document.getElementsByClassName('causecheck')).concat(Array.from(document.getElementsByClassName('skillcheck')));
-  checks = checks.filter(function(check){
-    return check.checked;
-  })
-
-  checks = checks.map(function(check){
-    return check.nextSibling.innerHTML
-  })
-
-  var removedCards = [];
-
-  for (let i = 0; i<cards.length; i++) {
-      var mycardblock = cards[i].querySelector('.card-block')
-      var mytags = Array.from(mycardblock.querySelectorAll('.causetag')).concat(Array.from(mycardblock.querySelectorAll('.skilltag')));
-      var toRemove = false;
-      for (let j=0; j< mytags.length; j++){
-          console.log(mytags)
-          console.log(checks)
-          if (!checks.includes(mytags[j].innerHTML)){
-              toRemove = true;
-              console.log('hello')
-              break;
-          }
-      }
-      if(toRemove){
-          removedCards.push(cards[i])
-      }
-  }
-  console.log(removedCards)
-  for(var k= 0; k<removedCards; k++){
-      removedCards[k].style.display = None;
->>>>>>> 51846b81ce2907febd54aca5955e17353b233c48
     }
 }
 
