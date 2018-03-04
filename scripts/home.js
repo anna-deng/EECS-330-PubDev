09f0e38d5c795cdbdd49e0d8e93b4d0f6dcc93c0
rohitrastogi <rohitrastogi2019@u.northwestern.edu>
2018-03-04 17:57:12 -0600

    fixed homepage login/signup bug

--- scripts/home.js
+++ scripts/home.js
@@ -62,10 +62,25 @@ function validateLogin(){
             email.parentElement.insertBefore(emailValidation, email.nextSibling);
         }
     }
-
 }
 
 function isValidEmail(email){
     let re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
     return re.test(String(email).toLowerCase());
 }
+
+function removeElementsByClass(className){
+    var elements = document.getElementsByClassName(className);
+    while(elements.length > 0){
+        elements[0].parentNode.removeChild(elements[0]);
+    }
+}
+
+$(function(){
+    $('#signup').on('hide.bs.modal', function(e){
+        removeElementsByClass('text-danger');
+    })
+    $('#login').on('hide.bs.modal', function(e){
+        removeElementsByClass('text-danger');
+    })
+})
