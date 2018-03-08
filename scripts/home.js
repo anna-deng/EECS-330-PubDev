e2c8356f7c245807e22f0b75b91eda19477db1b1
Tara Ramanathan <tararamanathan2020@gmail.com>
2018-03-07 20:38:39 -0600

    Make navbar visible after passing buttons

--- scripts/home.js
+++ scripts/home.js
@@ -1,3 +1,26 @@
+(function ($) {
+  $(document).ready(function(){
+
+    // hide .navbar first
+    $(".navbar").hide();
+
+    // fade in .navbar
+    $(function () {
+        $(window).scroll(function () {
+
+                 // set distance user needs to scroll before we start fadeIn
+            if ($(this).scrollTop() > 550) {
+                $('.navbar').fadeIn();
+            } else {
+                $('.navbar').fadeOut();
+            }
+        });
+    });
+
+});
+  }(jQuery));
+
+
 function validateSignup(){
     let name = document.getElementById('signup-name');
     let email = document.getElementById('signup-email');
