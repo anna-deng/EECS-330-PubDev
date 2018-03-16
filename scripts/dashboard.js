c5f038851fdb953349323ee94cc1528d4ba220b8
Hayden Udelson <haydenudelson18@gmail.com>
2018-03-16 03:56:55 -0500

    Fixed text on dashboards

--- scripts/dashboard.js
+++ scripts/dashboard.js
@@ -8,6 +8,16 @@ function finishTask(task, button) {
 	taskList = taskList.removeChild(task);
 }
 
+function finishTask2(task, button) {
+	var btn = document.getElementById(button);
+	var task = document.getElementById(task);
+	var taskList = document.getElementById('taskList2');
+	btn.setAttribute("class", "btn btn-primary disabled");
+	btn.setAttribute("style", "background-color: #ee0979; border-color: #ee0979;")
+	btn.innerHTML = "Thanks!";
+	taskList = taskList.removeChild(task);
+}
+
 function claimTask(task, button) {
 	var btn = document.getElementById(button);
 	var task = document.getElementById(task);
@@ -25,6 +35,25 @@ function claimTask(task, button) {
 		btn.setAttribute("style", "background-color: #ee0979; border-color: #ee0979;")
 	}	
 }
+
+function claimTask2(task, button) {
+	var btn = document.getElementById(button);
+	var task = document.getElementById(task);
+	var taskList = document.getElementById('taskList2');
+
+	if (btn.innerHTML == "Claim Task") {
+		btn.innerHTML = "Task Claimed!";
+		taskList = taskList.appendChild(task);
+		btn.setAttribute("class", "btn btn-primary disabled");
+		btn.setAttribute("style", "background-color: #ee0979; border-color: #ee0979;")
+	} else if (btn.innerHTML == "Task Finished"){
+		taskList = taskList.removeChild(task);
+		btn.innerHTML = "Thanks!";
+		btn.setAttribute("class", "btn btn-primary disabled");
+		btn.setAttribute("style", "background-color: #ee0979; border-color: #ee0979;")
+	}	
+}
+
 
 function changeButton(button) {
 	var btn = document.getElementById(button);
@@ -61,6 +90,28 @@ function scrollCard() {
 		card1.setAttribute("style", "display: none");
 		card2.setAttribute("style", "display: block");
 	}
+}
 
-	
+function approved(button, button1, modal) {
+	var btn = document.getElementById(button);
+	var btn1 = document.getElementById(button1);
+	var modal = document.getElementById(modal);
+	var taskList = document.getElementById('approvalQueue');
+	btn.innerHTML = "Approved!";
+	btn.setAttribute("class", "btn btn-primary disabled");
+	btn.setAttribute("style", "background-color: #ee0979; border-color: #ee0979; width: 150px");
+	btn1.setAttribute("style", "display: none;");
+	taskList.removeChild(modal);
+}
+
+function denied(button, button1, modal) {
+	var btn = document.getElementById(button);
+	var btn1 = document.getElementById(button1);
+	var modal = document.getElementById(modal);
+	var taskList = document.getElementById('approvalQueue');
+	btn.innerHTML = "Denied!";
+	btn.setAttribute("class", "btn btn-primary disabled");
+	btn.setAttribute("style", "background-color: #ee0979; border-color: #ee0979; width: 150px");
+	btn1.setAttribute("style", "display: none;");
+	taskList.removeChild(modal);
 }
